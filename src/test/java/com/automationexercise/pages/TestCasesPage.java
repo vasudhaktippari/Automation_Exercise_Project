33ad9bc1b7a20dc21dd87058ff4a2bfd8d7ad392
Nayeem Laheji <132880575+nayeem003@users.noreply.github.com>
2025-09-05 01:38:02 +0530

    Update TestCasesPage.java

--- src/test/java/com/automationexercise/pages/TestCasesPage.java
+++ src/test/java/com/automationexercise/pages/TestCasesPage.java
@@ -9,7 +9,7 @@ public class TestCasesPage {
 
     private WebDriver driver;
 
-    // Locators
+   
     private By testScenarioList = By.cssSelector(".panel-heading a"); // links to scenarios
     private By testStepsSection = By.cssSelector(".panel-body");      // steps of selected scenario
     private By feedbackLink = By.xpath("//a[contains(text(),'Contact us')]");
@@ -17,11 +17,20 @@ public class TestCasesPage {
     private By subscriptionBtn = By.id("subscribe");
     private By successMsg = By.cssSelector(".alert-success");
 
+    
+    private By productsLink = By.cssSelector("a[href='/products'] i");
+    private By cartLink = By.cssSelector("a[href='/view_cart'] i");
+    private By apiTestingLink = By.cssSelector("a[href='/api_list'] i");
+
+    private By apiTestingHeading = By.xpath("/html/body/section/div/div[1]/div/h2/b");
+    private By api1Link = By.xpath("/html/body/section/div/div[2]/div/div[1]/h4/a/u");
+    private By api1Content = By.xpath("/html/body/section/div/div[2]/div/div[2]/ul/li[1]/a");
+
     public TestCasesPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    // Actions
+    // =================== Existing Actions ===================
     public void openTestCasesPage() {
         driver.get("https://automationexercise.com/test_cases");
     }
@@ -57,5 +66,50 @@ public class TestCasesPage {
 
     public boolean isSubscriptionSuccess() {
         return driver.findElement(successMsg).isDisplayed();
+    }
+
+    // =================== New Actions ===================
+
+    // 🔹 Products Page
+    public boolean isProductsLinkVisible() {
+        return driver.findElement(productsLink).isDisplayed();
+    }
+
+    public void clickProductsLink() {
+        driver.findElement(By.cssSelector("a[href='/products']")).click();
+    }
+
+    // 🔹 Cart Page
+    public boolean isCartLinkVisible() {
+        return driver.findElement(cartLink).isDisplayed();
+    }
+
+    public void clickCartLink() {
+        driver.findElement(By.cssSelector("a[href='/view_cart']")).click();
+    }
+
+    // 🔹 API Testing Page
+    public boolean isAPITestingLinkVisible() {
+        return driver.findElement(apiTestingLink).isDisplayed();
+    }
+
+    public void clickAPITestingLink() {
+        driver.findElement(By.cssSelector("a[href='/api_list']")).click();
+    }
+
+    public boolean isAPITestingHeadingVisible() {
+        return driver.findElement(apiTestingHeading).isDisplayed();
+    }
+
+    public boolean isAPI1Visible() {
+        return driver.findElement(api1Link).isDisplayed();
+    }
+
+    public void clickAPI1() {
+        driver.findElement(api1Link).click();
+    }
+
+    public boolean isAPI1ContentVisible() {
+        return driver.findElement(api1Content).isDisplayed();
     }
 }
